511aa9615a56defd24e1a61098bfe3c09661b2d1
Julius <julius@Sage-plant.local>
2020-01-30 18:56:05 +0100

    attempt 1

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -1,58 +1,96 @@
 #include <iostream>
 #include <random>
+#include <stdlib.h>
 #include "TrafficLight.h"
 
 /* Implementation of class "MessageQueue" */
-
-/* 
+ 
 template <typename T>
 T MessageQueue<T>::receive()
 {
-    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
-    // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+    std::unique_lock<std::mutex> lock(_mutex);
+
+    _condition.wait(lock, [this]{return !_queue.empty(); });
+    T msg = std::move(_queue.back());
+    _queue.pop_back();
+
+    return msg;
 }
 
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
-    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
-    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> lock(_mutex);
+    _queue.push_back(std::move(msg));
+    _condition.notify_one();
 }
-*/
+
 
 /* Implementation of class "TrafficLight" */
 
-/* 
-TrafficLight::TrafficLight()
-{
-    _currentPhase = TrafficLightPhase::red;
+TrafficLight::TrafficLight() :_currentPhase(TrafficLightPhase::red) {
+        _type = ObjectType::objectTrafficLight;
 }
 
 void TrafficLight::waitForGreen()
 {
-    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
-    // runs and repeatedly calls the receive function on the message queue. 
-    // Once it receives TrafficLightPhase::green, the method returns.
+    while(true){
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        TrafficLightPhase phase = _lightQueue.receive();
+        if (phase == TrafficLightPhase::green)
+            return;
+    }
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
 {
+    std::lock_guard<std::mutex> lock(_mutex);
     return _currentPhase;
+}
+
+void TrafficLight::setCurrentPhase(TrafficLightPhase && phase)
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    _currentPhase = std::move(phase);
 }
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
 void TrafficLight::cycleThroughPhases()
 {
-    // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
-    // and toggles the current phase of the traffic light between red and green and sends an update method 
-    // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+ 
+    srand (time(NULL));
+    int cycleLength = rand() % 2000 + 4000;
+
+    std::chrono::time_point<std::chrono::system_clock> lastUpdate;
+    lastUpdate = std::chrono::system_clock::now();
+
+    while(true){
+        // compute time difference to stop watch
+        long timeSinceLastUpdate =
+            std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()
+                    - lastUpdate).count(); 
+        if (timeSinceLastUpdate > cycleLength){ 
+            switch (getCurrentPhase()){
+                case TrafficLightPhase::green:
+                    setCurrentPhase(TrafficLightPhase::red);
+                    _lightQueue.send(TrafficLightPhase::red); 
+                    break;
+                case red:
+                    setCurrentPhase(TrafficLightPhase::green);
+                    _lightQueue.send(TrafficLightPhase::green); 
+                    break;
+            }
+            lastUpdate = std::chrono::system_clock::now();
+            cycleLength = rand() % 2000 + 4000;
+        }
+        // sleep at every iteration to reduce CPU usage
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
 }
 
-*/
